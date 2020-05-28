9fe1ada0feb26adc1ebbaa72f7042d06df737259
FelipeTomazEC <felipetomaz2206@gmail.com>
2020-05-28 17:41:06 -0300

    #feat: Add Libraries and Playlists to left side bar

--- src/components/LeftSideBar/index.js
+++ src/components/LeftSideBar/index.js
@@ -1,11 +1,35 @@
 import React from "react";
 import { Navigator } from "./Navigator";
+import { SideList } from "./SideList";
 import { Container } from "./styles";
 
 export function LeftSideBar({ className }) {
+  const libraries = [
+    "Made For You",
+    "Recently Played",
+    "Liked Songs",
+    "Albums",
+    "Artists",
+    "Podcasts",
+  ];
+
+  const playlists = [
+    "This is Coldplay",
+    "This is Khalid",
+    "This is Queen",
+    "Workout",
+    "This is The Chainsmokers",
+    "Time to sleep",
+    "Top America",
+    "Rap God",
+    "Life Sucks",
+  ];
+
   return (
     <Container className={className}>
       <Navigator />
+      <SideList items={libraries} title="Your Library" />
+      <SideList items={playlists} title="Playlists" />
     </Container>
   );
 }
