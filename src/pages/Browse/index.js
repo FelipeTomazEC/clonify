cf8ddfceb0441fc6dd550986e4a15096275cfc11
FelipeTomazEC <felipetomaz2206@gmail.com>
2020-05-28 15:58:47 -0300

    #feat: Create empty content pages

---
+++ src/pages/Browse/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Browse() {
+  return <h1>Browse Page</h1>;
+}
---
+++ src/pages/Radio/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Radio() {
+  return <h1>Radio Page</h1>;
+}
