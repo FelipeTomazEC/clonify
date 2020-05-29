c475abcf44980b461b4e2749d492b9fdde6bbb69
FelipeTomazEC <felipetomaz2206@gmail.com>
2020-05-29 17:00:49 -0300

    #feat: Create page header

---
+++ src/components/StickyPageHeader/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useInView } from "react-intersection-observer";
+import { Container, StickySentinel } from "./styles";
+
+export function StickyPageHeader({ title }) {
+  const [ref, inView] = useInView({ threshold: 0 });
+
+  return (
+    <>
+      <StickySentinel ref={ref} paddingTop={100} />
+      <Container isSticked={inView}>
+        <h1>{title}</h1>
+      </Container>
+    </>
+  );
+}
---
+++ src/components/StickyPageHeader/styles.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  width: 100%;
+  height: 3em;
+  background-color: ${(props) => (props.isSticked ? "transparent" : "#000")};
+  padding-left: 30px;
+  position: sticky;
+  top: 0;
+
+  h1 {
+    color: #fff;
+    font-size: ${(props) => (props.isSticked ? 3 : 1.8)}em;
+    font-weight: 800;
+  }
+`;
+
+export const StickySentinel = styled.div`
+  background: #403f3b;
+  background: -moz-linear-gradient(top, #403f3b 0%, #181817 51%, #181817 100%);
+  background: -webkit-linear-gradient(
+    top,
+    #403f3b 0%,
+    #181817 51%,
+    #181817 100%
+  );
+  background: linear-gradient(to bottom, #403f3b 0%, #181817 51%, #181817 100%);
+  width: 100%;
+  height: ${(props) => props.paddingTop}px;
+`;
