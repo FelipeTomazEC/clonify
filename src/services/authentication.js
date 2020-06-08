72960981f3788b0cdc3a0a5f2116be63ff91d413
FelipeTomazEC <felipetomaz2206@gmail.com>
2020-06-08 11:56:17 -0300

    #feat: Create authentication related functions

---
+++ src/services/authentication.js
@@ -0,0 +1,39 @@
+export function getSpotifyOAuthURL() {
+  const endpoint = "https://accounts.spotify.com/authorize";
+  const client_id = "33d7584571144836b02e538bd4119af6";
+  const redirect_uri = "http://192.168.120.110:3000/authentication-callback";
+
+  return `${endpoint}/?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=token`;
+}
+
+export function setSession(accessToken, tokenTimeToLive, callback) {
+  const expiresIn = Date.now() + tokenTimeToLive * 1000;
+  const credentials = JSON.stringify({ accessToken, expiresIn });
+
+  localStorage.setItem("@spotify/credentials", credentials);
+
+  callback();
+}
+
+export function clearSession(callback) {
+  localStorage.removeItem("@spotify/credentials");
+
+  callback();
+}
+
+export function getToken() {
+  const credentials =
+    JSON.parse(localStorage.getItem("@spotify/credentials")) || {};
+
+  const accessToken = credentials.accessToken;
+
+  return accessToken;
+}
+
+export function isLogged() {
+  const credentials = JSON.parse(localStorage.getItem("@spotify/credentials"));
+
+  if (!credentials) return false;
+
+  return credentials.expiresIn > Date.now();
+}
