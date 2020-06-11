c1b19131bdc5ae0cf893e7f674f26fb1e350dc84
FelipeTomazEC <felipetomaz2206@gmail.com>
2020-06-10 22:30:57 -0300

    #feat: Add scope param to oauth request url

--- src/services/authentication.js
+++ src/services/authentication.js
@@ -2,8 +2,12 @@ export function getSpotifyOAuthURL() {
   const endpoint = "https://accounts.spotify.com/authorize";
   const client_id = "33d7584571144836b02e538bd4119af6";
   const redirect_uri = "http://192.168.120.110:3000/authentication-callback";
+  const scopes = ["user-follow-read", "user-library-read"];
 
-  return `${endpoint}/?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=token`;
+  return `${endpoint}/?client_id=${client_id}`
+    .concat(`&redirect_uri=${redirect_uri}`)
+    .concat(`&response_type=token`)
+    .concat(`&scopes=${encodeURIComponent(scopes.join(" "))}`);
 }
 
 export function setSession(accessToken, tokenTimeToLive, callback) {
