9ac6d4caab053afd8998dbe67fe14b3faf2b870d
FelipeTomazEC <felipetomaz2206@gmail.com>
2020-06-12 18:22:27 -0300

    #feat: Create podcast entity

---
+++ src/entities/podcast.js
@@ -0,0 +1,9 @@
+export class Podcast {
+  constructor({ name, id, description, covers, publisher }) {
+    this.name = name;
+    this.id = id;
+    this.description = description;
+    this.covers = covers;
+    this.publisher = publisher;
+  }
+}
