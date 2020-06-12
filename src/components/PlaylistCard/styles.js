887171edf5de669f0e3b52c203cf599f076520e9
FelipeTomazEC <felipetomaz2206@gmail.com>
2020-06-12 18:52:50 -0300

    #fix: Set text overflow to avoid broken layout

--- src/components/PlaylistCard/styles.js
+++ src/components/PlaylistCard/styles.js
@@ -1,30 +1,42 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
   font-size: 0.9rem;
   color: #b2afae;
   line-height: 1rem;
+  overflow: hidden;
+
+  > * {
+    display: block;
+  }
+
+  .description,
+  .name {
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    overflow: hidden;
+    -webkit-line-clamp: 3; /* number of lines to show */
+    -webkit-box-orient: vertical;
+  }
+
+  .name {
+    margin-top: 0.5rem;
+    cursor: pointer;
+    color: #fff;
+    text-transform: capitalize;
+    -webkit-line-clamp: 2; /* number of lines to show */
+    margin-bottom: 0.3rem;
+
+    :hover {
+      text-decoration: underline;
+      text-underline-position: below;
+    }
+  }
 
   .followers-number {
     text-transform: uppercase;
     letter-spacing: 1px;
     font-weight: 100;
     margin-top: 0.3rem;
-  }
-
-  .name {
-    margin-top: 0.5rem;
-    cursor: pointer;
-    width: fit-content;
-    color: #fff;
-    text-transform: capitalize;
-    margin-bottom: 0.3rem;
-
-    :hover {
-      text-decoration: underline;
-      text-underline-position: under;
-    }
   }
 `;
