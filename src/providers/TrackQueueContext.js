2039d86171f349984a9ef73646c9ad9924b73a92
FelipeTomazEC <felipetomaz2206@gmail.com>
2020-06-15 16:32:34 -0300

    #refactor: Change initial state to an track instance

--- src/providers/TrackQueueContext.js
+++ src/providers/TrackQueueContext.js
@@ -1,27 +1,27 @@
 import React, { createContext, useState } from "react";
-
+import { Artist } from "../entities/artist";
+import { Track } from "../entities/track";
 export const TrackQueueContext = createContext();
 
 export function TrackQueueProvider(props) {
   const [queue, setQueue] = useState([
-    {
-      title: "Awesome Music",
-      artist: "John Mac",
-      cover:
+    new Track({
+      albumCover:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBYxXx88UgZe3s4dmgyxFdI1z6OReq8DAokR4JPVv-moaly2B4&usqp=CAU",
-      isLiked: true,
+      albumId: "some-fake-id",
+      albumName: "Awesome Album",
+      artists: [
+        new Artist({
+          avatar: "artist.com/avatar.png",
+          id: "fake-id",
+          name: "Mrs. Awesome",
+        }),
+      ],
+      id: "fake-track-id",
       sourceUrl:
         "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3",
-    },
-    {
-      title: "It's play time",
-      artist: "Funny Bill",
-      cover:
-        "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/techno-triangle-album-cover-flyer-template-2f2a9d4851c7de5f4f2362d3352f42fc.jpg?ts=1561427894",
-      isLiked: false,
-      sourceUrl:
-        "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3",
-    },
+      title: "Awesome Music",
+    }),
   ]);
 
   return (
