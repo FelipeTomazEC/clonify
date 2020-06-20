b80532f4a5bf8f7bc3d8b58ad0f0f745d4567ba8
FelipeTomazEC <felipetomaz2206@gmail.com>
2020-06-19 21:00:25 -0300

    #fix: Title blinking in edge cases

--- src/components/StickyPageHeader/index.js
+++ src/components/StickyPageHeader/index.js
@@ -2,15 +2,18 @@ import React from "react";
 import { useInView } from "react-intersection-observer";
 import { Container, StickySentinel } from "./styles";
 
-export function StickyPageHeader({ title, paddingTop = 100 }) {
-  const [ref, inView] = useInView({ threshold: 0 });
+export function StickyPageHeader({ title, children }) {
+  const [ref, isSentinelInView] = useInView({ threshold: 0 });
 
   return (
-    <>
-      <StickySentinel ref={ref} paddingTop={paddingTop} />
-      <Container isSticked={!inView}>
-        <h1>{title}</h1>
+    <React.Fragment>
+      <StickySentinel ref={ref} />
+      <Container>
+        <div className="wrapper" stuck={isSentinelInView ? "false" : "true"}>
+          <h1 className="title">{title}</h1>
+          {children}
+        </div>
       </Container>
-    </>
+    </React.Fragment>
   );
 }
--- src/components/StickyPageHeader/styles.js
+++ src/components/StickyPageHeader/styles.js
@@ -1,31 +1,46 @@
 import styled from "styled-components";
 
-export const Container = styled.header`
-  width: 100%;
-  height: 3em;
-  background-color: ${(props) => (props.isSticked ? "#0c0c0c" : "transparent")};
-  position: sticky;
-  top: 0;
-
-  h1 {
-    color: #fff;
-    font-size: ${(props) => (props.isSticked ? 1.8 : 3)}em;
-    font-weight: 800;
-    border-bottom: ${(props) =>
-      props.isSticked ? "rgba(178, 175, 174, 0.2) solid 1px" : "none"};
-  }
+export const StickySentinel = styled.div`
+  display: block;
+  min-height: 100px;
 `;
 
-export const StickySentinel = styled.div`
-  background: #403f3b;
-  background: -moz-linear-gradient(top, #403f3b 0%, #181817 51%, #181817 100%);
-  background: -webkit-linear-gradient(
-    top,
-    #403f3b 0%,
-    #181817 51%,
-    #181817 100%
-  );
-  background: linear-gradient(to bottom, #403f3b 0%, #181817 51%, #181817 100%);
-  width: 100%;
-  min-height: ${(props) => props.paddingTop}px;
+export const Container = styled.div`
+  color: white;
+  top: 0;
+  position: sticky;
+  font-weight: 800;
+  z-index: 10000;
+
+  > * {
+    padding-left: 30px;
+  }
+
+  .wrapper {
+    display: block;
+
+    > .title {
+      line-height: 3.5rem;
+      height: 3.5rem;
+      font-size: 3.3rem;
+    }
+  }
+
+  .wrapper[stuck="true"] {
+    animation: background 300ms forwards;
+
+    > .title {
+      font-size: 1.8rem;
+    }
+
+    @keyframes background {
+      from {
+        background-color: transparent;
+      }
+      to {
+        background-color: #0c0c0c;
+        border-bottom: rgba(178, 175, 174, 0.2) solid 1px;
+      }
+    }
+  }
 `;
