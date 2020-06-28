1226952a573109d9859eeedb5ef9f216b1cb34d0
FelipeTomazEC <felipetomaz2206@gmail.com>
2020-06-28 03:49:03 -0300

    #feat: Create page to view albums

---
+++ src/pages/Application/AlbumView/index.js
@@ -0,0 +1,109 @@
+import React, { useContext, useEffect, useState } from "react";
+import { BsClock, BsHeartFill, BsThreeDots, BsVolumeUp } from "react-icons/bs";
+import { FaPlay } from "react-icons/fa";
+import { useInView } from "react-intersection-observer";
+import { useParams } from "react-router-dom";
+import { ContentLoadingAnimation } from "../../../components/ContentLoadingAnimation";
+import { HeaderAlbumView } from "../../../components/HeaderAlbumView";
+import { PlayerContext } from "../../../providers/player-context";
+import { CHANGE_QUEUE, PLAY_TRACK } from "../../../reducers/player-reducer";
+import { getMultipleAlbumsFromAPI } from "../../../services/get-album-from-api";
+import { AnimationContainer, Container } from "./styles";
+
+export function AlbumView() {
+  const [ref, isSentinelInView] = useInView({ threshold: 0 });
+  const { id } = useParams();
+  const [album, setAlbum] = useState(null);
+  const [player, dispatch] = useContext(PlayerContext);
+
+  useEffect(() => {
+    getMultipleAlbumsFromAPI([id])
+      .then(([album]) => setAlbum(album))
+      .catch((err) => console.error(err));
+  }, [id]);
+
+  const handleTrackClick = (trackIndex) => {
+    dispatch({ type: CHANGE_QUEUE, queue: album.tracks });
+    dispatch({ type: PLAY_TRACK, trackIndex });
+  };
+
+  const isActive = (track) => {
+    const { queue, currentPlayingIndex } = player;
+    const currentTrack = queue[currentPlayingIndex];
+
+    return track.id === currentTrack?.id;
+  };
+
+  const getTimeFromMilliseconds = (timeInMilliseconds) => {
+    const format2Digit = (value) => `${value}`.padStart(2, "0");
+    const timeInSeconds = Math.round(timeInMilliseconds / 1000);
+    const hours = Math.floor(timeInSeconds / 3600);
+    const minutes = Math.floor((timeInSeconds % 3600) / 60);
+    const seconds = timeInSeconds - minutes * 60 - hours * 3600;
+
+    return hours > 0
+      ? `${hours}:${format2Digit(minutes)}:${format2Digit(seconds)}`
+      : `${minutes}:${format2Digit(seconds)}`;
+  };
+
+  return album === null ? (
+    <AnimationContainer>
+      <ContentLoadingAnimation />
+    </AnimationContainer>
+  ) : (
+    <Container>
+      <div ref={ref} className="sticky-guard"></div>
+      <HeaderAlbumView album={album} compact={!isSentinelInView} />
+      <section className="tracks-section">
+        <table className="tracks-table">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th />
+              <th>Title</th>
+              <th>Artist</th>
+              <th />
+              <th>
+                <BsClock />
+              </th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {album.tracks.map((track, index) => (
+              <tr key={track.id} active={isActive(track).toString()}>
+                <td className="col-play-button">
+                  <span>{index + 1}</span>
+                  <button onClick={() => handleTrackClick(index)}>
+                    {isActive(track) ? (
+                      <BsVolumeUp size={15} />
+                    ) : (
+                      <FaPlay size={10} />
+                    )}
+                  </button>
+                </td>
+                <td className="col-like-button">
+                  <button>
+                    <BsHeartFill size={15} />
+                  </button>
+                </td>
+                <td className="col-title">{track.title}</td>
+                <td className="col-artist">
+                  {track.artists.map((a) => a.name).join(", ")}
+                </td>
+                <td className="col-more-button">
+                  <button>
+                    <BsThreeDots size={20} />
+                  </button>
+                </td>
+                <td className="col-duration">
+                  {getTimeFromMilliseconds(track.duration)}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </section>
+    </Container>
+  );
+}
---
+++ src/pages/Application/AlbumView/styles.js
@@ -0,0 +1,171 @@
+import styled from "styled-components";
+
+export const AnimationContainer = styled.div`
+  max-width: 100%;
+  height: 100%;
+  overflow-y: auto;
+  background: #403f3b;
+  background: -moz-linear-gradient(top, #403f3b 0%, #181817 10%, #181817 100%);
+  background: -webkit-linear-gradient(
+    top,
+    #403f3b 0%,
+    #181817 10%,
+    #181817 100%
+  );
+  background: linear-gradient(to bottom, #403f3b 0%, #181817 10%, #181817 100%);
+`;
+
+export const Container = styled.div`
+  position: relative;
+  max-width: 100%;
+  height: 100%;
+  max-height: 100%;
+  overflow-y: auto;
+  background: #403f3b;
+  background: -moz-linear-gradient(top, #403f3b 0%, #181817 10%, #181817 100%);
+  background: -webkit-linear-gradient(
+    top,
+    #403f3b 0%,
+    #181817 10%,
+    #181817 100%
+  );
+  background: linear-gradient(to bottom, #403f3b 0%, #181817 10%, #181817 100%);
+
+  /*
+    Guard to control how the header is presented when stuck.
+  */
+  .sticky-guard {
+    position: absolute;
+    top: 0;
+    z-index: 2;
+    height: 150px;
+    width: 100%;
+  }
+
+  .tracks-section {
+    margin-top: 270px;
+    display: block;
+    width: 100%;
+    color: #fff;
+    padding: 0px 32px;
+
+    table {
+      width: 100%;
+      border-collapse: collapse;
+      font-size: 0.9rem;
+
+      th,
+      td {
+        border-bottom: #282828 solid 1px;
+        text-align: left;
+        vertical-align: center;
+        max-width: 0px;
+        height: 40px;
+        padding: 0px 15px;
+      }
+
+      th {
+        text-transform: uppercase;
+        font-weight: 200;
+        color: #b2afae;
+        letter-spacing: 1px;
+      }
+
+      td {
+        text-overflow: ellipsis;
+        overflow: hidden;
+        white-space: nowrap;
+      }
+
+      tr {
+        :hover {
+          background-color: #282828;
+
+          > .col-play-button,
+          .col-more-button {
+            button {
+              display: inline-block;
+            }
+          }
+
+          > .col-play-button {
+            span {
+              display: none;
+            }
+          }
+        }
+      }
+
+      tr[active="true"] {
+        color: #1db954;
+
+        .col-play-button {
+          span {
+            display: none;
+          }
+          button {
+            display: block;
+          }
+        }
+      }
+
+      .col-like-button,
+      .col-play-button,
+      .col-more-button {
+        padding: 0;
+        width: 35px;
+        height: 100%;
+        text-align: center;
+
+        button {
+          background-color: transparent;
+          border: none;
+          line-height: 50%;
+          outline: none;
+          color: #fff;
+          width: 35px;
+          height: 35px;
+
+          :hover {
+            transform: scale(1.1);
+          }
+        }
+      }
+
+      .col-play-button {
+        position: relative;
+
+        button {
+          background-color: #181817;
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translateX(-50%) translateY(-50%);
+          border-radius: 50%;
+          border: #fff 1px solid;
+          width: 28px;
+          height: 28px;
+
+          :hover {
+            transform: translateX(-50%) translateY(-50%) scale(1.1);
+          }
+        }
+      }
+
+      .col-play-button,
+      .col-more-button {
+        button {
+          display: none;
+        }
+      }
+
+      .col-title {
+        width: 45%;
+      }
+
+      .col-artist {
+        width: 30%;
+      }
+    }
+  }
+`;
