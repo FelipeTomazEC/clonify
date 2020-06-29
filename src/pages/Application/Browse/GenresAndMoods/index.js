df3288524b51c5ee6e16e2a36e28d9dcd3a8a7dd
FelipeTomazEC <felipetomaz2206@gmail.com>
2020-06-29 15:48:13 -0300

    #feat:Add loading indicator

--- src/pages/Application/Browse/GenresAndMoods/index.js
+++ src/pages/Application/Browse/GenresAndMoods/index.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
+import { ContentLoadingAnimation } from "../../../../components/ContentLoadingAnimation";
 import { GenreAndMoodCard } from "../../../../components/GenreAndMoodCard";
 import { getGenresFromAPI } from "../../../../services/get-genres-from-api";
 import { Container } from "./styles";
 
 export function GenresAndMoods() {
-  const [genres, setGenres] = useState([]);
+  const [genres, setGenres] = useState(null);
 
   useEffect(() => {
     getGenresFromAPI()
@@ -14,18 +15,24 @@ export function GenresAndMoods() {
 
   return (
     <Container>
-      <h2 className="title">Genres & Moods</h2>
-      <ul className="genres-list">
-        {genres.map((genre) => (
-          <li className="genre-item" key={genre.id}>
-            <GenreAndMoodCard
-              cover={genre.cover}
-              name={genre.name}
-              onClick={() => alert(`Click on ${genre.id}`)}
-            />
-          </li>
-        ))}
-      </ul>
+      {genres === null ? (
+        <ContentLoadingAnimation />
+      ) : (
+        <Fragment>
+          <h2 className="title">Genres & Moods</h2>
+          <ul className="genres-list">
+            {genres.map((genre) => (
+              <li className="genre-item" key={genre.id}>
+                <GenreAndMoodCard
+                  cover={genre.cover}
+                  name={genre.name}
+                  onClick={() => alert(`Click on ${genre.id}`)}
+                />
+              </li>
+            ))}
+          </ul>
+        </Fragment>
+      )}
     </Container>
   );
 }
