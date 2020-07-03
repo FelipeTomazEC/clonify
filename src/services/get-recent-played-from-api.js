51478d09cdb3f2865025bf78eb3f7086d1fb4818
FelipeTomazEC <felipetomaz2206@gmail.com>
2020-07-03 00:39:16 -0300

    #fix: App not loading when album or playlist is missing from recently played list

--- src/services/get-recent-played-from-api.js
+++ src/services/get-recent-played-from-api.js
@@ -5,35 +5,24 @@ import { fetchFromApi } from "../services/spotify-web-api-service";
 export async function getRecentPlayedFromAPI() {
   const endpoint = "/me/player/recently-played?limit=50";
   const data = await fetchFromApi(endpoint);
-  const contexts = data.items.flatMap((i) =>
-    i.context && (i.context.type === "album" || i.context.type === "playlist")
-      ? [i.context]
-      : []
+
+  const contexts = data.items.flatMap((i) => {
+    const { context } = i;
+    if (!context) return [];
+    if (context.type !== "album" && context.type !== "playlist") return [];
+
+    const id = context.href.split("/").pop();
+    const type = context.type;
+    return [[id, type]];
+  });
+
+  const withoutDuplicates = [...new Map(contexts).entries()];
+
+  const recentPlayed = await Promise.all(
+    withoutDuplicates.map(([id, type]) =>
+      type === "album" ? getMultipleAlbumsFromAPI([id]) : getPlaylistFromAPI(id)
+    )
   );
 
-  const getIdFromUrl = (url) => {
-    const parts = url.split("/");
-    return parts[parts.length - 1];
-  };
-
-  const playlistsIds = new Set(
-    contexts
-      .filter((ctx) => ctx.type === "playlist")
-      .map((ctx) => getIdFromUrl(ctx.href))
-  );
-
-  const albumsIds = contexts
-    .filter((ctx) => ctx.type === "album")
-    .map((ctx) => getIdFromUrl(ctx.href));
-
-  const playlists = await Promise.all(
-    [...playlistsIds].map(getPlaylistFromAPI)
-  );
-
-  const albums = await getMultipleAlbumsFromAPI([...new Set(albumsIds)]);
-
-  console.log("Albums", albums);
-  console.log("Playlists", playlists);
-
-  return [...playlists, ...albums];
+  return recentPlayed;
 }
