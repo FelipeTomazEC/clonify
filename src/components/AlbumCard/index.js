3524115a9f12f98d9e21accaf79ea3ba08f37d93
FelipeTomazEC <felipetomaz2206@gmail.com>
2020-07-17 11:18:09 -0300

    #fix: Start album playing on click

--- src/components/AlbumCard/index.js
+++ src/components/AlbumCard/index.js
@@ -1,17 +1,21 @@
-import React, { useContext } from "react";
-import { NavLink } from "react-router-dom";
-import { PlayerContext } from "../../providers/player-context";
-import { CHANGE_QUEUE } from "../../reducers/player-reducer";
-import { PlayableCard } from "../PlayableCard";
-import { Container } from "./styles";
+import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
+import { PlayerContext } from '../../providers/player-context';
+import { CHANGE_QUEUE, PLAY_TRACK } from '../../reducers/player-reducer';
+import { PlayableCard } from '../PlayableCard';
+import { Container } from './styles';
 
 export function AlbumCard({ album }) {
   const { name, artists, cover, isLiked = false, tracks } = album;
+
   const [, dispatch] = useContext(PlayerContext);
 
-  const artistsNames = artists.map((a) => a.name).join(", ");
+  const artistsNames = artists.map((a) => a.name).join(', ');
 
-  const handlePlayClick = () => dispatch({ type: CHANGE_QUEUE, queue: tracks });
+  const handlePlayClick = () => {
+    dispatch({ type: CHANGE_QUEUE, queue: tracks });
+    dispatch({ type: PLAY_TRACK, trackIndex: 0 });
+  };
 
   return (
     <Container>
