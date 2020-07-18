c0f2dc6acfc998687d683f3b674cb09d6ec5e080
FelipeTomazEC <felipetomaz2206@gmail.com>
2020-07-18 16:30:40 -0300

    #refactor: Migrate AlbumCard component to typescript

--- src/components/AlbumCard/index.tsx
+++ src/components/AlbumCard/index.tsx
@@ -1,33 +1,38 @@
 import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
+import { Album } from '../../entities/album';
 import { PlayerContext } from '../../providers/player-context';
 import { CHANGE_QUEUE, PLAY_TRACK } from '../../reducers/player-reducer';
 import { PlayableCard } from '../PlayableCard';
 import { Container } from './styles';
 
-export function AlbumCard({ album }) {
-  const { name, artists, cover, isLiked = false, tracks } = album;
+interface Props {
+  album: Album;
+}
 
+export function AlbumCard(props: Props) {
   const [, dispatch] = useContext(PlayerContext);
 
-  const artistsNames = artists.map((a) => a.name).join(', ');
-
   const handlePlayClick = () => {
-    dispatch({ type: CHANGE_QUEUE, queue: tracks });
+    dispatch({ type: CHANGE_QUEUE, queue: props.album.tracks });
     dispatch({ type: PLAY_TRACK, trackIndex: 0 });
   };
 
   return (
     <Container>
       <PlayableCard
-        coverUrl={cover}
-        isLiked={isLiked}
+        coverUrl={props.album.cover}
+        isLiked={false}
         handlePlayClick={handlePlayClick}
       />
-      <NavLink to={`/application/albums/${album.id}`}>
-        <strong className="name">{name}</strong>
+
+      <NavLink to={`/application/albums/${props.album.id}`}>
+        <strong className="name">{props.album.name}</strong>
       </NavLink>
-      <span className="artist">{artistsNames}</span>
+
+      <span className="artist">
+        {props.album.artists.map((a) => a.name).join(', ')}
+      </span>
     </Container>
   );
 }
