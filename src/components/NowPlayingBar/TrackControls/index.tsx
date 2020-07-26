e17b09193ea898f00eece8bb35d0c595c2c9b49e
FelipeTomazEC <felipetomaz2206@gmail.com>
2020-07-26 00:07:46 -0300

    #refactor: Rewrite Track Controls component in typescript

--- src/components/NowPlayingBar/TrackControls/index.tsx
+++ src/components/NowPlayingBar/TrackControls/index.tsx
@@ -1,30 +1,39 @@
-import React, { useContext, useEffect, useState } from "react";
-import { BsSkipEndFill, BsSkipStartFill } from "react-icons/bs";
-import { FaRandom, FaRegPauseCircle, FaRegPlayCircle } from "react-icons/fa";
-import { FiRepeat } from "react-icons/fi";
-import { PlayerContext } from "../../../providers/player-context";
-import { PLAY_TRACK } from "../../../reducers/player-reducer";
-import { Container } from "./styles";
+import React, { useContext, useEffect, useState } from 'react';
+import { BsSkipEndFill, BsSkipStartFill } from 'react-icons/bs';
+import { FaRandom, FaRegPauseCircle, FaRegPlayCircle } from 'react-icons/fa';
+import { FiRepeat } from 'react-icons/fi';
+import { Player } from '../../../entities/player';
+import { PlayerContext } from '../../../providers/player-context';
+import { PLAY_TRACK } from '../../../reducers/player-reducer';
+import { InputEvent } from '../../../types/input-event.type';
+import { Container } from './styles';
 
 export function TrackControls() {
-  const [player, dispatch] = useContext(PlayerContext);
+  const [player, dispatch]: [
+    Player,
+    (action: { type: string; trackIndex: number }) => void
+  ] = useContext(PlayerContext);
+
   const [currentTime, setCurrentTime] = useState(0);
   const [duration, setDuration] = useState(0);
 
-  const audioTrack = player.currentPlayingAudio;
-
   useEffect(() => {
+    const audioTrack = player.currentPlayingAudio;
     if (audioTrack) {
       audioTrack.ontimeupdate = () => setCurrentTime(audioTrack.currentTime);
       audioTrack.onloadedmetadata = () => setDuration(audioTrack.duration);
 
       audioTrack.play();
-
-      return (prev) => (prev ? prev.pause() : null);
     }
-  }, [audioTrack]);
+
+    return () => {
+      audioTrack?.pause();
+      return;
+    };
+  }, [player.currentPlayingAudio]);
 
   const handlePlayPauseClick = () => {
+    const audioTrack = player.currentPlayingAudio;
     const isPlaying = !audioTrack?.paused;
 
     return isPlaying ? audioTrack?.pause() : audioTrack?.play();
@@ -44,24 +53,28 @@ export function TrackControls() {
     dispatch({ type: PLAY_TRACK, trackIndex: nextIndex });
   };
 
-  const changeProgress = (e) => {
-    const newProgress = e.target.value;
+  const changeProgress = (e: InputEvent) => {
+    const audioTrack = player.currentPlayingAudio as HTMLAudioElement;
+    const newProgress = e.target.valueAsNumber;
     audioTrack.currentTime = newProgress;
   };
 
   const restartFromTheBeginning = () => {
+    const audioTrack = player.currentPlayingAudio as HTMLAudioElement;
     audioTrack.currentTime = 0;
   };
 
-  const getTimeInMinutes = (timeInSeconds) => {
+  const getTimeInMinutes = (timeInSeconds: number) => {
     const seconds = Math.round(timeInSeconds) % 60;
     const minutes = Math.floor(timeInSeconds / 60);
 
-    return `${minutes}:${"".concat(seconds).padStart(2, 0)}`.split(".")[0];
+    return `${minutes}:${seconds.toString().padStart(2, '0')}`.split('.')[0];
   };
 
   const PlayPauseButtonIcon =
-    audioTrack?.paused || !audioTrack ? FaRegPlayCircle : FaRegPauseCircle;
+    player.currentPlayingAudio?.paused || !player.currentPlayingAudio
+      ? FaRegPlayCircle
+      : FaRegPauseCircle;
 
   return (
     <Container>
