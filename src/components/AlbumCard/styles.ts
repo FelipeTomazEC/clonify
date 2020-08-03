e254a98d1351e0b040169c70e78315731806f7ab
FelipeTomazEC <felipetomaz2206@gmail.com>
2020-08-03 17:27:30 -0300

    #fix: Specify width due broken layout

--- src/components/AlbumCard/styles.ts
+++ src/components/AlbumCard/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
@@ -6,19 +6,24 @@ export const Container = styled.div`
   font-size: 0.9rem;
   color: #b2afae;
   line-height: 1rem;
+  width: 180px;
 
   a:link {
     text-decoration: none;
   }
 
   .name {
-    display: block;
     margin-top: 0.5rem;
     cursor: pointer;
     width: fit-content;
     color: #fff;
     text-transform: capitalize;
     margin-bottom: 0.3rem;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 2; /* number of lines to show */
+    -webkit-box-orient: vertical;
 
     :hover {
       text-decoration: underline;
