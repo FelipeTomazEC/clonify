5267d860fb03aa89933bfb8439963e68b7692d36
FelipeTomazEC <felipetomaz2206@gmail.com>
2020-12-27 12:58:13 -0300

    Refactor[WIP]: Reformulate the playlist page

--- src/pages/Application/PlaylistView/index.tsx
+++ src/pages/Application/PlaylistView/index.tsx
@@ -1,117 +1,75 @@
-import React, { Fragment, useContext, useEffect, useState } from 'react';
-import { BsHeartFill, BsThreeDots, BsVolumeUp } from 'react-icons/bs';
-import { FaPlay } from 'react-icons/fa';
-import { RiCalendar2Line } from 'react-icons/ri';
-import { useParams } from 'react-router-dom';
-import { ContentLoadingAnimation } from '../../../components/ContentLoadingAnimation';
-import { PlaylistViewHeader } from '../../../components/PlaylistViewHeader';
-import { Playlist } from '../../../entities/playlist';
-import { Track } from '../../../entities/track';
-import { PlayerContext } from '../../../providers/player-context';
-import { PlayerActionType } from '../../../reducers/player-reducer';
-import { getPlaylistFromAPI } from '../../../services/get-playlist-from-api';
-import { Container } from './styles';
+import React, {useEffect, useState} from 'react';
+import {useParams} from 'react-router-dom';
+import {ContentLoadingAnimation} from '../../../components/ContentLoadingAnimation';
+import {Playlist} from '../../../entities/playlist';
+import {getPlaylistFromAPI} from '../../../services/get-playlist-from-api';
+import {Container, Wrapper, StickyGuard, Header, Cover, Info, Label, Name, Description, Owner } from './styles';
+import {usePlayer} from "../../../hooks/use-player";
+import {PlaylistTrackTable} from "../../../components/PlaylistTrackTable";
+import {ApplicationStatus} from "../../../constants/application-status.enum";
+import {useInView} from "react-intersection-observer";
+import {Time} from "../../../utils/time";
+import {TimeWithUnitsFormatter} from "../../../utils/time-with-units-formatter";
+import {ButtonsBar} from "../../../components/ButtonsBar";
+
 
 export function PlaylistView() {
-  const { id } = useParams<{ id: string }>();
+  const {id} = useParams<{ id: string }>();
+  const [ref, isGuardInView] = useInView({ threshold: 0 });
   const [playlist, setPlaylist] = useState<Playlist>();
-  const [player, dispatch] = useContext(PlayerContext);
+  const [status, setStatus] = useState<ApplicationStatus>(ApplicationStatus.LOADING);
+  const {playTrack, tracks, setTracks, activeTrackId} = usePlayer([]);
+  const time = Time.parseMillisecondsToTime(
+      tracks.reduce((acc, cur) => acc + cur.duration, 0)
+  );
+  const formatter = new TimeWithUnitsFormatter();
 
   useEffect(() => {
-    getPlaylistFromAPI(id)
-      .then((playlist) => setPlaylist(playlist))
-      .catch((err) => console.error(err));
-  }, [id]);
+    const fetchPlaylist = async () => {
+      try {
+        const playlist = await getPlaylistFromAPI(id);
+        setPlaylist(playlist);
+        setTracks(playlist.tracks);
+        setStatus(ApplicationStatus.READY);
+      } catch (err) {
+        console.error(err);
+        setStatus(ApplicationStatus.SOMETHING_WENT_WRONG);
+      }
+    };
 
-  const handleTrackClick = (track: Track) => {
-    if (!playlist) return;
-
-    const trackIndex = playlist.tracks.indexOf(track);
-
-    dispatch({
-      type: PlayerActionType.CHANGE_QUEUE,
-      payload: { queue: playlist.tracks },
-    });
-
-    dispatch({
-      type: PlayerActionType.PLAY_TRACK,
-      payload: { trackIndex },
-    });
-  };
-
-  const isActive = (track: Track) => {
-    const { queue, currentPlayingIndex } = player;
-    const currentTrack = queue[currentPlayingIndex];
-
-    return track.id === currentTrack?.id;
-  };
-
-  const removeDuplicates = (tracks: Track[]) => {
-    const trackMap = tracks.reduce(
-      (map, track) => map.set(track.id, track),
-      new Map<string, Track>()
-    );
-
-    return [...trackMap.values()];
-  };
+    fetchPlaylist();
+  }, [id, setTracks]);
 
   return (
-    <Container>
-      {playlist === undefined ? (
-        <ContentLoadingAnimation className="loading-animation" />
-      ) : (
-        <Fragment>
-          <PlaylistViewHeader playlist={playlist} />
-          <section className="tracks-section">
-            <table className="tracks-table">
-              <thead>
-                <tr>
-                  <th />
-                  <th />
-                  <th>Title</th>
-                  <th>Artist</th>
-                  <th>
-                    <RiCalendar2Line />
-                  </th>
-                </tr>
-              </thead>
+      <Container>
+        {
+          status === ApplicationStatus.LOADING &&
+          <ContentLoadingAnimation className="loading-animation"/>
+        }
 
-              <tbody>
-                {removeDuplicates(playlist.tracks).map((track) => (
-                  <tr
-                    key={track.id}
-                    {...{ active: isActive(track).toString() }}
-                  >
-                    <td className="col-play-button">
-                      <button onClick={() => handleTrackClick(track)}>
-                        {isActive(track) ? (
-                          <BsVolumeUp size={15} />
-                        ) : (
-                          <FaPlay size={10} />
-                        )}
-                      </button>
-                    </td>
-                    <td className="col-like-button">
-                      <button>
-                        <BsHeartFill size={15} />
-                      </button>
-                    </td>
-                    <td className="col-title">{track.title}</td>
-                    <td className="col-artist">
-                      {track.artists.map((a) => a.name).join(', ')}
-                    </td>
-                    <td className="col-more-button">
-                      <button>
-                        <BsThreeDots size={20} />
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </section>
-        </Fragment>
-      )}
-    </Container>
+        {
+          status === ApplicationStatus.READY &&
+          <>
+              <Header>
+                  <StickyGuard ref={ref}/>
+                  <Wrapper isStuck={!isGuardInView}>
+                      <Cover src={playlist?.cover} isStuck={!isGuardInView}/>
+                      <Info isStuck={!isGuardInView}>
+                          <Label className='label'>Playlist</Label>
+                          <Name isStuck={!isGuardInView}>{playlist?.name}</Name>
+                          <Description className='description'>{playlist?.description}</Description>
+                          <Owner className='owner'>Created by &nbsp;
+                              <strong>{playlist?.ownerName}</strong>
+                          </Owner>
+                          <span className='tracks-info'>{tracks.length} {tracks.length === 1 ? 'song' : 'songs'}, &nbsp;</span>
+                          <span className='time-info'>{formatter.format(time)}</span>
+                      </Info>
+                      <ButtonsBar></ButtonsBar>
+                  </Wrapper>
+              </Header>
+              <PlaylistTrackTable tracks={tracks} playTrack={playTrack} activeTrackId={activeTrackId}/>
+          </>
+        }
+      </Container>
   );
 }
--- src/pages/Application/PlaylistView/styles.ts
+++ src/pages/Application/PlaylistView/styles.ts
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
+interface StickyContainerContent {
+  isStuck: boolean;
+}
+
 export const Container = styled.div`
   position: relative;
   max-width: 100%;
@@ -8,12 +12,10 @@ export const Container = styled.div`
   overflow-y: auto;
   background: #403f3b;
   background: -moz-linear-gradient(top, #403f3b 0%, #181817 10%, #181817 100%);
-  background: -webkit-linear-gradient(
-    top,
-    #403f3b 0%,
-    #181817 10%,
-    #181817 100%
-  );
+  background: -webkit-linear-gradient(top,
+  #403f3b 0%,
+  #181817 10%,
+  #181817 100%);
   background: linear-gradient(to bottom, #403f3b 0%, #181817 10%, #181817 100%);
 
   .loading-animation {
@@ -21,141 +23,105 @@ export const Container = styled.div`
     top: 50%;
     transform: translateY(-50%);
   }
+`;
 
-  /*
-    Guard to control how the header is presented when stuck.
-  */
-  .sticky-guard {
-    position: absolute;
-    top: 0;
-    z-index: 2;
-    height: 150px;
-    width: 100%;
+export const Wrapper = styled.div<StickyContainerContent>`
+  position: ${props => props.isStuck ? 'sticky' : 'absolute'};
+  display: flex;
+  align-items: center;
+  top: 0;
+  width: 100%;
+  height: ${props => props.isStuck ? '70px' : '250px'};
+  background-color: red;
+  animation: ${props => props.isStuck ? 'collapsing' : 'expanding'} 300ms forwards;
+
+  @keyframes collapsing {
+    from {
+      background-color: transparent;
+    }
+    to {
+      background-color: #0C0C0B;
+      border-bottom: rgba(178, 175, 174, 0.2) solid 1px;
+    }
   }
 
-  .tracks-section {
-    margin-top: 270px;
-    display: block;
-    width: 100%;
-    color: #fff;
-    padding: 0px 32px;
-
-    table {
-      width: 100%;
-      border-collapse: collapse;
-      font-size: 0.9rem;
-
-      th,
-      td {
-        border-bottom: #282828 solid 1px;
-        text-align: left;
-        vertical-align: center;
-        max-width: 0px;
-        height: 40px;
-        padding: 0px 15px;
-      }
-
-      th {
-        text-transform: uppercase;
-        font-weight: 200;
-        color: #b2afae;
-        letter-spacing: 1px;
-      }
-
-      td {
-        text-overflow: ellipsis;
-        overflow: hidden;
-        white-space: nowrap;
-      }
-
-      tr {
-        :hover {
-          background-color: #282828;
-
-          > .col-play-button,
-          .col-more-button {
-            button {
-              display: inline-block;
-            }
-          }
-        }
-      }
-
-      tr[active="true"] {
-        color: #1db954;
-
-        .col-play-button {
-          button {
-            display: block;
-          }
-        }
-      }
-
-      .col-like-button,
-      .col-play-button,
-      .col-more-button {
-        padding: 0;
-        width: 35px;
-        height: 100%;
-
-        button {
-          background-color: transparent;
-          border: none;
-          line-height: 50%;
-          outline: none;
-          color: #fff;
-          width: 35px;
-          height: 35px;
-
-          :hover {
-            transform: scale(1.1);
-          }
-        }
-      }
-
-      .col-play-button {
-        position: relative;
-
-        button {
-          background-color: #181817;
-          position: absolute;
-          top: 50%;
-          left: 50%;
-          transform: translateX(-50%) translateY(-50%);
-          border-radius: 50%;
-          border: #fff 1px solid;
-          width: 28px;
-          height: 28px;
-
-          :hover {
-            transform: translateX(-50%) translateY(-50%) scale(1.1);
-          }
-        }
-      }
-
-      .col-play-button,
-      .col-more-button {
-        button {
-          display: none;
-        }
-      }
-
-      .col-more-button {
-        width: 100px;
-
-        button {
-          width: 35px;
-          margin-left: 65px;
-        }
-      }
-
-      .col-title {
-        width: 45%;
-      }
-
-      .col-artist {
-        width: 30%;
-      }
+  @keyframes expanding {
+    from {
+      background-color: #0C0C0B;
+      border-bottom: rgba(178, 175, 174, 0.2) solid 1px;
+    }
+    to {
+      background-color: transparent;
     }
   }
 `;
+
+export const StickyGuard = styled.div`
+  width: 100%;
+  height: 120px;
+`;
+
+export const Header = styled.header`
+  min-height: 250px;
+  position: sticky;
+  top: -121px;
+`;
+
+export const Cover = styled.img<StickyContainerContent>`
+  width: ${props => props.isStuck ? '40px' : '220px'};
+  height: ${props => props.isStuck ? '40px' : '220px'};
+  margin: 15px 30px;
+`;
+
+export const Info = styled.div<StickyContainerContent>`
+  color: #939090;
+  font-size: 0.95rem;
+  
+  ${props => props.isStuck ? `
+    .description, .label, .tracks-info, .time-info, .owner {
+      display: none;
+    }
+  ` : ''}
+`;
+
+export const Label = styled.h3`
+  color: #fff;
+  font-size: 0.9rem;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: 200;
+`;
+
+export const Name = styled.h1<StickyContainerContent>`
+  color: #fff;
+  font-size: ${props => props.isStuck ? '2rem' : '3rem'};
+  line-height: ${props => props.isStuck ? '2rem' : '3rem'};
+`;
+
+export const Description = styled.p`
+  display: block;
+`;
+
+export const Owner = styled.span`
+  display: inline-flex;
+  align-items: center;
+
+  strong {
+    color: #fff;
+  }
+
+  ::after {
+    content: '';
+    margin: 0 0.3rem;
+    display: inline-block;
+    height: 1px;
+    width: 1px;
+    background-color: #939090;
+    border: #939090 solid 2px;
+    border-radius: 100%;
+  }
+`;
+
+
+
+
