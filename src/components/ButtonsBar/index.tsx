c7013fe80df964c441ddb70a4585c9a8c215459e
FelipeTomazEC <felipetomaz2206@gmail.com>
2020-12-27 12:57:10 -0300

    Feature[WIP]: Create a header button bar (Play, More and Like button).

---
+++ src/components/ButtonsBar/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Container, PlayButton} from './styles'
+
+export function ButtonsBar() {
+  return (
+      <Container>
+        <PlayButton>Play</PlayButton>
+      </Container>
+  );
+}
---
+++ src/components/ButtonsBar/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const PlayButton = styled.button`
+  width: 150px;
+  height: 35px;
+  font-size: 1.1rem;
+  color: #fff;
+  border: none;
+  border-radius: 20px;
+  text-transform: uppercase;
+  background-color: #1DB954;
+`;
